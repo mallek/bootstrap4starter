9e88418cf3e25863c5d38cd67fc99a50fb169ce5
mallek <travis@haleycomputersolutions.com>
2017-11-23 09:10:23 -0700

    added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+//compile Sass & Inject Into Browser
+gulp.task('sass', function () {
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('js', function () {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+        .pipe(gulp.dest("src/js"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['sass'], function () {
+    browserSync.init({
+        server: "./src"
+    });
+
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['scss']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+
+});
+
+//Move Font awesome fonts folder to src/fonts
+gulp.task('fonts', function () {
+    return gulp.src('node_modules/font-awseome/fonts/*')
+        .pipe(gulp.dest("src/fonts"));
+});
+
+//Move Font Awesome css folder to src/css
+gulp.task('fa', function () {
+    return gulp.src('node_modules/font-awseome/css/font-awesome.min.css')
+        .pipe(gulp.dest("src/css"));
+});
+
+gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
